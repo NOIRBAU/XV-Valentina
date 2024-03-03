4934c4da68d0afd8abb56bbe2904c416f8310ab1
Noirbau <nelly.ale.bg@gmail.com>
2024-03-03 14:32:58 -0600

    cancion

---
+++ loader.js
@@ -0,0 +1,11 @@
+/* PAGINA DE CARGA */
+
+window.addEventListener('load', () => {
+    const loader = document.getElementById("loader");
+        loader.classList.add('loader-finish');
+});
+
+//const loader = document.getElementById("loader");
+//window.addEventListener("load",function(){
+//    loader.style.display = "none";
+//})
